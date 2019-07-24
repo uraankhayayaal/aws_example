144f260b4799ca7093d7a36809b1fd7523b23920
uraankhayayaal <uraankhayayaal@gmail.com>
2019-07-24 02:06:49 +0000

    auth added

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_IGwkRHBqu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6a2ds39etokvbui13vg50fg78k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
