f9f0690be7ca60fb507fead7dae60f0afefe3993
uraankhayayaal <uraankhayayaal@gmail.com>
2019-07-24 03:20:47 +0000

    ride invoke added

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://c3ni3hkfd1.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
-};
+}; // pwd: Hu`123Fg
